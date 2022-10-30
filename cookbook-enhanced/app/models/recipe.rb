3c6be9fedac2b50c1699dd936060900e13e1e559
Francesca Santoriello <f.santoriello@hotmail.com>
2022-10-30 19:44:28 +0100

    tested > starting p

--- cookbook-enhanced/app/models/recipe.rb
+++ cookbook-enhanced/app/models/recipe.rb
@@ -1,8 +1,5 @@
 require 'active_record'
 
 class Recipe < ActiveRecord::Base
-  ActiveRecord::Base.establish_connection(
-    adapter:  "sqlite3",
-    database: "../../db/development"
-  )
+  ActiveRecord::Base.establish_connection(adapter:  "sqlite3", database: "../../db/development")
 end
